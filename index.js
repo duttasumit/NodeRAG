2e12ed9630511c89890f7f12325926070358d53b
duttasumit <er.sumitdutta1991@gmail.com>
2024-12-13 23:20:36 +0530

    Maintaining chat history with RunnableWithMessageHistory

--- index.js
+++ index.js
@@ -1,65 +1,91 @@
-import { ChatGroq } from "@langchain/groq";
-import 'dotenv/config'
-
-const GROQ_API_KEY = process.env.GROQ_API_KEY
-
-const llm = new ChatGroq({
-    model: 'mixtral-8x7b-32768',
-    temperature: 0,
-    apiKey: GROQ_API_KEY
-})
-
 /* FOR RAG, we need:
 1. Some type of loader to load the data (pdfloader, webloader)
 2. Text splitter to split the text loaded from the loader (RecursiveCharaterTextSplitter)
 3. Embeddings to convert text into embeddings (OpenAIEmbeddings, OllamaEmbeddings)
 4. Vector DB to load the text embeddings (FAISS or Memory)
-5. Retrieval chain
-6. StuffDocument Chain
+5. Retrieval chain to recieve the documents and pass to the stuffdocument chain
+6. StuffDocument Chain to send list of documents to the model
 7. chatprompttemplate for question-answering chain.
+8. Messageplaceholder variable chat_history to manage history
+9. createhistoryawareretriever to manage when chat_history is empty, else chain
+10. Chatmessagehistory to manage session wise history
+11. RunnableWithMessageHistory handles injecting chat history into inputs
 */
 
-import { CheerioWebBaseLoader } from '@langchain/community/document_loaders/web/cheerio'
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
-import { MemoryVectorStore } from 'langchain/vectorstores/memory'
-import { ChatPromptTemplate } from '@langchain/core/prompts'
-import { createRetrievalChain } from 'langchain/chains/retrieval'
-import { createStuffDocumentsChain } from 'langchain/chains/combine_documents'
-import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/hf";
 
-// Loading from web and create doucments.
-const webloader = new CheerioWebBaseLoader(
+import { ChatGroq } from "@langchain/groq";
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/hf";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts"
+import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
+import { createRetrievalChain } from "langchain/chains/retrieval";
+import { createHistoryAwareRetriever } from "langchain/chains/history_aware_retriever";
+import { ChatMessageHistory } from "langchain/stores/message/in_memory";
+import { RunnableWithMessageHistory } from "@langchain/core/runnables";
+import "dotenv/config";
+
+
+// 1. Initialize the llm model.
+const llm = new ChatGroq({
+    model: 'mixtral-8x7b-32768',
+    temperature: 0,
+    apiKey: process.env.GROQ_API_KEY
+})
+
+
+// 2. Initialize the loader and load the content.
+const loader = new CheerioWebBaseLoader(
     "https://lilianweng.github.io/posts/2023-06-23-agent/",
     {
         selector: ".post-content, .post-title, .post-header",
     }
 )
-const docs = await webloader.load()
+const docs = await loader.load()
 
 
-// Splitting docs.
+// 3. Initialize the textsplitter and split the content (docs).
 const textsplitter = new RecursiveCharacterTextSplitter({
     chunkSize: 1000,
-    chunkOverlap: 50
+    chunkOverlap: 200
 })
 const splits = await textsplitter.splitDocuments(docs)
 
 
-// Creating Embeddings.
-const embeddings = new HuggingFaceInferenceEmbeddings({
-    model: 'sentence-transformers/all-MiniLM-L6-v2',
-    apiKey: process.env.HUGGINGFACEHUB_API_KEY,
-});
-
-// Store in vectorestore.
-const vectorestore = await MemoryVectorStore.fromDocuments(
-    splits,
-    embeddings
-)
-const retriever = vectorestore.asRetriever()
+// 4. Store the splits in the memoryvectorstore with embeddings.
+const hfembeddings = new HuggingFaceInferenceEmbeddings({
+    model: "sentence-transformers/all-MiniLM-L6-v2",
+    apiKey: process.env.HUGGINGFACEHUB_API_KEY
+})
+const store = await MemoryVectorStore.fromDocuments(splits, hfembeddings)
+const retriever = store.asRetriever()
 
 
-// Use retriever for QnA Chain.
+// 5. Contextualize the Question, use Chatprompttemplate.
+const contextualizeQSystemPrompt =
+  "Given a chat history and the latest user question " +
+  "which might reference context in the chat history, " +
+  "formulate a standalone question which can be understood " +
+  "without the chat history. Do NOT answer the question, " +
+  "just reformulate it if needed and otherwise return it as is.";
+
+const contextualizeQPrompt = ChatPromptTemplate.fromMessages([
+    ["system", contextualizeQSystemPrompt],
+    new MessagesPlaceholder("chat_history"),
+    ["human", "{input}"]
+])
+
+
+// 6. Use createHistoryRetriever to manage contexts and chaining.
+const historyAwareRetriever = await createHistoryAwareRetriever({
+    llm,
+    retriever,
+    rephrasePrompt: contextualizeQPrompt
+})
+
+
+// 7. Give system prompt to Answer Question.
 const systemPrompt =
   "You are an assistant for question-answering tasks. " +
   "Use the following pieces of retrieved context to answer " +
@@ -69,67 +95,51 @@ const systemPrompt =
   "\n\n" +
   "{context}";
 
-
-import { createHistoryAwareRetriever } from 'langchain/chains/history_aware_retriever'
-import { MessagesPlaceholder } from '@langchain/core/prompts'
-
-const contextualizeQSystemPrompt =
-  "Given a chat history and the latest user question " +
-  "which might reference context in the chat history, " +
-  "formulate a standalone question which can be understood " +
-  "without the chat history. Do NOT answer the question, " +
-  "just reformulate it if needed and otherwise return it as is.";
-
-
-const contextualizeQPrompt = ChatPromptTemplate.fromMessages([
-    ["system", contextualizeQSystemPrompt],
-    new MessagesPlaceholder('chat_history'),
-    ["human", "{input}"] 
-])
-
-const historyAwareRetriever = await createHistoryAwareRetriever({
-    llm,
-    retriever,
-    rephrasePrompt: contextualizeQPrompt
-})
-
-
 const qaPrompt = ChatPromptTemplate.fromMessages([
     ["system", systemPrompt],
-    new MessagesPlaceholder('chat_history'),
+    new MessagesPlaceholder("chat_history"),
     ["human", "{input}"]
 ])
 
-const questionAnswerChain2 = await createStuffDocumentsChain({
+
+// 8. Call createstuffdocumentchain to Pass the list of documents to the model.
+const qnachain = await createStuffDocumentsChain({
     llm,
     prompt: qaPrompt
 })
 
-const ragchain2 = await createRetrievalChain({
+
+// 9. Call createRetrievalChain to pass the documents to the stuffdocumentchain.
+const ragchain = await createRetrievalChain({
     retriever: historyAwareRetriever,
-    combineDocsChain: questionAnswerChain2
+    combineDocsChain: qnachain
 })
 
 
-import { HumanMessage, AIMessage } from '@langchain/core/messages'
+// 10. Manage chat history statefully.
+const sessionStore = {}
 
-let chatHistory = []
+function getSessionHistory(sessionId) {
+    if(!(sessionId in sessionStore)) {
+        sessionStore[sessionId] = new ChatMessageHistory()
+    }
+    return sessionStore[sessionId]
+}
 
-const question = "What is Task Decomposition?"
-const aiMsg1 = await ragchain2.invoke({
-    input: question,
-    chat_history: chatHistory
+// Initialize RunnableWithMessageHistory to handle chat history.
+const ragchainConversation = new RunnableWithMessageHistory({
+    runnable: ragchain,
+    getMessageHistory: getSessionHistory,
+    inputMessagesKey: "input",
+    historyMessagesKey: "chat_history",
+    outputMessagesKey: "answer"
 })
 
-chatHistory = chatHistory.concat([
-    new HumanMessage(question),
-    new AIMessage(aiMsg1.answer)
-])
 
-const secondQuestion = "What are common ways of doing it?"
-const aiMsg2 = await ragchain2.invoke({
-    input: secondQuestion,
-    chat_history: chatHistory
-})
+// 11. Use the model to query.
+const response = await ragchainConversation.invoke(
+    { input: "What is Task Decomposition?"},
+    { configurable: { sessionId: "abc123" }}
+)
 
-console.log(aiMsg2.answer)
+console.log(response.answer)
